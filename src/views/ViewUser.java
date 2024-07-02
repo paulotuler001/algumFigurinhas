9abed44d4b64222419470b35d058c3e67476a92a
Bella-Sa <bella.sa@gmail.com>
2024-07-02 18:40:33 -0300

    frm user 3 page doc 7 changes

--- src/views/ViewUser.java
+++ src/views/ViewUser.java
@@ -19,21 +19,24 @@ public class ViewUser extends JFrame{
 	    loginLabel.setBackground(Color.WHITE);
 		JTextField loginField = new JTextField(15);
 		loginField.setBackground(Color.WHITE);
+		
 		JLabel passwordLabel = new JLabel("Senha");
 		passwordLabel.setBackground(Color.WHITE);
-		JTextField passwordField = new JTextField(15);
+		JPasswordField passwordField = new JPasswordField(15);
 		passwordField.setBackground(Color.WHITE);
+		
+	    String[] profile = {"Author", "Collector", "Adiministrator"};
 		JLabel profileLabel = new JLabel("Perfil");
 		profileLabel.setBackground(Color.WHITE);
-		JTextField profileField = new JTextField(15);
-		profileField.setBackground(Color.WHITE);
-
+		JComboBox <String> dropdown = new JComboBox<>(profile);
+		dropdown.setBackground(Color.WHITE);
+		  
 		loginLabel.setBounds(yy, xx, 165, 25);
 		loginField.setBounds(yy+100, xx, 165, 25);
 		passwordLabel.setBounds(yy, xx+30, 165, 25);
 		passwordField.setBounds(yy+100, xx+30, 165, 25);
 		profileLabel.setBounds(yy+25, xx+90, 80, 25);
-		profileField.setBounds(yy+125, xx+90, 80, 25);
+		dropdown.setBounds(yy+125, xx+90, 80, 25);
 		 
 		 JButton okayrBtn = new JButton("okay");
 		 okayrBtn.setFocusable(false);
@@ -50,7 +53,7 @@ public class ViewUser extends JFrame{
 		 panel.add(passwordLabel);
 		 panel.add(passwordField);
 		 panel.add(profileLabel);
-		 panel.add(profileField);
+		 panel.add(dropdown);
 		 panel.add(okayrBtn);
 		 panel.add(cancelBtn);
 
